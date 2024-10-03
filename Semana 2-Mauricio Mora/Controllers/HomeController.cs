50e739aa216dcbb711b1df6107b390ca5360f00a
laboratorio <laboratorio@UPOAULA10302>
2024-10-03 09:05:02 -0500

    Agregar archivos de proyecto.

---
+++ Semana 2-Mauricio Mora/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Semana_2_Mauricio_Mora.Models;
+using System.Diagnostics;
+
+namespace Semana_2_Mauricio_Mora.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Semana 2-Mauricio Mora/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Semana_2_Mauricio_Mora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
